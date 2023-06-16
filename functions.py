6d2de37373f7fdcfc2cbbe020ceca639ad56961e
Mateo Membreno <125720626+summerlycow@users.noreply.github.com>
2023-06-15 22:18:27 -0400

    6/15

--- functions.py
+++ functions.py
@@ -4,9 +4,9 @@ import mysql.connector
 # Define the database connection function
 def establish_db_connection():
     conn = mysql.connector.connect(
-        host="your_host",
-        user="your_user",
-        password="your_password",
+        host="localhost",
+        user="root",
+        password=input("password?"),
         database="your_database"
     )
     return conn
@@ -21,9 +21,20 @@ def scrape_page(page_url):
 
 
 # Define the function to insert data into the database
-def insert_data_to_db(conn, data):
+def insert_match_data_to_db(conn, data):
     cursor = conn.cursor()
     # Execute SQL statements to insert data into the database
-    # ...
+    insert_query = "INSERT INTO your_table (match_id, date, duration, game_mode, team_1_score, team_2_score, " \
+                   "winning_team) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(insert_query, data)
+    conn.commit()
+    cursor.close()
+
+
+def insert_player_data_to_db(conn, data):
+    cursor = conn.cursor()
+    # Execute SQL statements to insert data into the database
+    insert_query = "INSERT INTO your_table (match_id, team_number, champion, player_number) VALUES (%s, %s, %s, %s, " \
+                   "%s, %s, %s)"
     conn.commit()
     cursor.close()
